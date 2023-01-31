f20765956e6427986c6f4fbae4d1fc5d4237904c
vihardiker <vihardik@microsoft.com>
2023-01-31 16:18:38 -0600

    13

--- providersandextensions.ps1
+++ providersandextensions.ps1
@@ -1,4 +1,14 @@
+# Login to Azure using Service Principal credentials from Github Secrets
+Write-Output "Logging in to Azure with a service principal..."
+az login --service-principal --username $Env:ARC_SP_CLIENT_ID --password $Env:ARC_SP_CLIENT_SECRET --tenant $Env:ARC_SP_TENANT_ID
+Write-Output "Done logging into Azure"
+
+# Select Azure subscription
+az account set --subscription $Env:ARC_AZURE_SUBSCRIPTION_ID
+
+# Get az version
 az --version 
+
 # Register the Arc resource providers
   az provider register --namespace Microsoft.Kubernetes
   az provider register --namespace Microsoft.KubernetesConfiguration
